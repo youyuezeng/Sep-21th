e06d06b7a6965553a6bf57e4b22ffd892a4ea1df
youyuezeng <113896465+youyuezeng@users.noreply.github.com>
2022-09-21 22:54:06 +0800

    Add files via upload

---
+++ Sep 21th/Sep 21th/9 21.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*#include <stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	printf("八进制：%#o", a);
+	printf("十六进制:%#X", a);
+	return 0;
+}*/
+/*#include <stdio.h>
+int main()
+{
+	int a= 0XABCDEF;
+	printf("%15d", a);
+	return 0;
+}*/
+/*#include <stdio.h>
+int main()
+{
+	int a=printf("Hello world!\n");
+	printf("%d",a);
+	
+}*/
+/*#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	a = b = c = 0;
+	scanf("%d%d%d", &a, &b, &c);
+	printf("score_a=%d score_b=%d score_c=%d", a, b, c);
+	return 0;
+}*/
+#include <stdio.h>
+int main()
+{
+	int a = 200406;
+	float C = 88.61531;
+	float Math = 51.5541464646848;
+	float English = 100.00;
+	printf("%d %.2f %.2f %.2f", a, C, Math, English);
+
+	return 0;
+}
